14a47aa22ca723548e6c89a621e3a274b20490a0
Sayuri Arake <sayuriarake@gmail.com>
2021-04-23 21:01:20 -0400

    feat: Criação dos widgets do aplicativo
    Tela home e quiz

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:devquiz/home/widgets/app_bar/app_bar_widget.dart';
+import 'package:devquiz/home/widgets/level_button/level_button_widget.dart';
+import 'package:devquiz/home/widgets/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,6 +15,33 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarWidget(),
+      body: Column(children: [
+        SizedBox(
+          height: 24,
+        ),
+        Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+          LevelButtonWidget(label: "Fácil"),
+          LevelButtonWidget(label: "Médio"),
+          LevelButtonWidget(label: "Difícil"),
+          LevelButtonWidget(label: "Perito")
+        ]),
+        SizedBox(
+          height: 24,
+        ),
+        Expanded(
+          child: GridView.count(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: [
+              QuizCardWidget(),
+              QuizCardWidget(),
+              QuizCardWidget(),
+              QuizCardWidget()
+            ],
+          ),
+        )
+      ]),
     );
   }
 }
